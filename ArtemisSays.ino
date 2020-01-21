af486335d7436f70bc1db73cfa07d1f4582dfeb9
Dillon <ee.tinkerer@gmail.com>
2020-01-20 20:00:21 -0500

    clean up comments

--- ArtemisSays.ino
+++ ArtemisSays.ino
@@ -1,107 +1,60 @@
 /******************************************************************************
-  MicroOLED_Hello.ino
-  SFE_MicroOLED Hello World Demo
-  Joel Bartlett @ SparkFun Electronics
-  Original Creation Date: January 20, 2015
+  ArtemisSays.ino
+  Dillon Nichols
   
-  This sketch shows you how to draw a 64x48 pixel Bitmap image on the MicroOLED.
+  This sketch draws to the 64x48 pixel MicroOLED
   
   Hardware Connections:
-    We'll be using the SPI interface on the MicroOLED, though it
-    also supports I2C (and a really messy parallel). If you want
-    to swap in I2C, read through the comments to find out how.
-    
-    MicroOLED ------------- Arduino
-      GND ------------------- GND
-      VDD ------------------- 3.3V (VCC)
-    D1/MOSI ----------------- D11 (don't change)
-    D0/SCK ------------------ D13 (don't change)
-      D2
-      D/C ------------------- D8 (can be any digital pin)
-      RST ------------------- D9 (can be any digital pin)
-      CS  ------------------- D10 (can be any digital pin)
-    
-  Development environment specifics:
-  	IDE: Arduino 1.0.5
-  	Hardware Platform: MicroOLED Breakout
-                           Arduino Pro 3.3V/8MHz
-  
-  Note: The display on the MicroOLED is a 1.8V-3.3V device only.
-  Don't try to connect a 5V Arduino directly to it! Use level
-  shifters in between the data signals if you have to resort to
-  a 5V board.
-  
-  This code is beerware; if you see me (or any other SparkFun 
-  employee) at the local, and you've found our code helpful, 
-  please buy us a round!
-  
-  Distributed as-is; no warranty is given.
+    We'll be using the Qwiic I2C interface on the MicroOLED
 *******************************************************************************/
-#include <Wire.h>  // Include Wire if you're using I2C
-#include <SPI.h>  // Include SPI if you're using SPI
-#include <SFE_MicroOLED.h>  // Include the SFE_MicroOLED library
+#include <Wire.h>
+#include <SFE_MicroOLED.h>
 #include "images.h"
 
 //////////////////////////
 // MicroOLED Definition //
 //////////////////////////
-#define PIN_RESET 9  // Connect RST to pin 9 (req. for SPI and I2C)
-#define PIN_DC    8  // Connect DC to pin 8 (required for SPI)
-#define PIN_CS    10 // Connect CS to pin 10 (required for SPI)
-#define DC_JUMPER 1 // Set to either 0 (SPI, default) or 1 (I2C) based on jumper, matching the value of the DC Jumper
-// Also connect pin 13 to SCK and pin 11 to MOSI
+// The library assumes a reset pin is necessary
+// The Qwiic OLED has RST hard-wired, so pick an arbitrary IO pin that is not being used
+#define PIN_RESET 9  
+// The DC_JUMPER is the I2C Address Select jumper
+// Set to 1 if the jumper is open (Default), or set to 0 if it's closed
+#define DC_JUMPER 1 
 
 //////////////////////////////////
 // MicroOLED Object Declaration //
 //////////////////////////////////
-// Declare a MicroOLED object. The parameters include:
-// 1 - Reset pin: Any digital pin
-// 2 - D/C pin: Any digital pin (SPI mode only)
-// 3 - CS pin: Any digital pin (SPI mode only, 10 recommended)
+// I2C declaration
+// I2C will result in a very slow update rate
+MicroOLED oled(PIN_RESET, DC_JUMPER);
 
-//MicroOLED oled(PIN_RESET, PIN_DC, PIN_CS); //Example SPI declaration, comment out if using I2C
-MicroOLED oled(PIN_RESET, DC_JUMPER); //Example I2C declaration, uncomment if using I2C 
-
-// I2C is great, but will result in a much slower update rate. The
-// slower framerate may be a worthwhile tradeoff, if you need more
-// pins, though.
-
-
-
-// I2C is great, but will result in a much slower update rate. The
-// slower framerate may be a worthwhile tradeoff, if you need more
-// pins, though.
 void setup()
 {
-
   delay(100);
-  Wire.begin(); //set up I2C bus, uncomment if you are using I2C
-  // These three lines of code are all you need to initialize the
-  // OLED and print the splash screen.
   
-  // These three lines of code are all you need to initialize the
-  // OLED and print the splash screen.
-  
-  // Before you can start using the OLED, call begin() to init
-  // all of the pins and configure the OLED.
-  oled.begin();
-  // clear(ALL) will clear out the OLED's graphic memory.
-  // clear(PAGE) will clear the Arduino's display buffer.
-  oled.clear(ALL);  // Clear the display's memory (gets rid of artifacts)
-  // To actually draw anything on the display, you must call the
-  // display() function. 
+  // Set up I2C bus
+  Wire.begin();
 
-  oled.setFontType(1);  // Set font to type 1
+  // Init all of the pins and configure the OLED
+  oled.begin();
+   
+  // clear(ALL) will clear out the OLED's graphic memory (gets rid of artifacts)
+  // clear(PAGE) will clear the Arduino's display buffer
+  oled.clear(ALL);
+
+  // Display text
+  oled.setFontType(1);
   oled.clear(PAGE);
   oled.setCursor(0, 1);
   oled.print("Artemis");
   oled.print("  Says");
   oled.display();
- 
-  delay(3000);//pause for the splash screen
-  //Note: You can change the spalsh screen by editing the array founf in the library source code
+
+  // Pause for the splash screen
+  delay(3000);
 }
 
+// Test all patterns by rotating through them
 void loop()
 {
   drawUp();
